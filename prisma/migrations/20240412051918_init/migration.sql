5eae10600b0fc8a5c34892910bf28f17d8eecf53
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2024-04-12 10:52:06 +0530

    supabase

---
+++ prisma/migrations/20240412051918_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "dummies" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "dummies_pkey" PRIMARY KEY ("id")
+);
